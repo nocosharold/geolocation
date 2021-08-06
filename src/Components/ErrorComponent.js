5880c321e415f829b64f310f31f636b2a06695e3
nocosharold <nocosharold@gmail.com>
2021-08-06 12:00:14 +0800

    Conditional Rendering Assignment

--- src/Components/ErrorComponent.js
+++ src/Components/ErrorComponent.js
@@ -5,7 +5,8 @@ class ErrorComponent extends React.Component{
     render(){
         return(
             <>
-                <h1>Error Handling {this.props.error}</h1>
+                {/* Conditional Rendering */}
+                {this.props.error !== null &&  <h1>Error Handling {this.props.error}</h1>}
                 <p>Latitude: { this.props.latitude }</p>
                 <p>Longitude: { this.props.longitude }</p>
             </>
